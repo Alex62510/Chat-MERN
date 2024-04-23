842377819f654cdf4f77d6e2320ac249de8df552
lex62510 <eagle625@tut.by>
2024-04-23 21:35:11 +0300

    create backend

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from "./routes/auth.routes.js";
 
-const app = express()
-dotenv.config()
 
-const PORT=process.env.PORT || 5000
+const app = express();
+dotenv.config();
 
-app.get('/',(req,res)=>{
-    res.send('Server is ready')
-})
+const PORT = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+app.use('/api/auth', authRoutes)
 
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`
-    )
-})
+    console.log(`Server is running on port ${PORT}`);
+});
