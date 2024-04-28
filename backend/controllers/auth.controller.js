7a0b52c928e0bee398c5baf7a3b0fcff67c03bbc
lex62510 <eagle625@tut.by>
2024-04-28 22:08:49 +0300

    add login controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,8 +2,25 @@ import User from "../models/user.models.js";
 import bcrypt from 'bcryptjs'
 import generateTokenAndSetCookie from "../utils/generateToken.js";
 
-export const login = (req, res) => {
-    console.log('loginUser')
+export const login = async (req, res) => {
+    try {
+        const {username, password} = req.body
+        const user = await User.findOne({username})
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || '')
+        if (!user || !isPasswordCorrect) {
+            return res.status(400).json({error: "Invalid username or password"})
+        }
+        generateTokenAndSetCookie(user._id, res)
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            boyProfilePic: user.profilePic
+        })
+    } catch (e) {
+        console.log("Error in login controller", e.message)
+        res.status(500).json({error: "Internal server Error"})
+    }
 }
 
 export const logout = (req, res) => {
@@ -21,8 +38,8 @@ export const signup = async (req, res) => {
             return res.status(400).json({error: "Username is already exists"})
         }
 
-        const salt=await bcrypt.genSalt(10)
-        const hashedPassword=await bcrypt.hash(password,salt)
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
 
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
         const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
@@ -30,21 +47,26 @@ export const signup = async (req, res) => {
         const newUser = await User({
             fullName,
             username,
-            password:hashedPassword,
+            password: hashedPassword,
             gender,
             profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
         })
 
-        if(newUser){
-            await generateTokenAndSetCookie(newUser._id,res)
+        if (newUser) {
+            await generateTokenAndSetCookie(newUser._id, res)
             await newUser.save()
-            res.status(201).json({_id:newUser._id, fullName:newUser.fullName, username:newUser.username,boyProfilePic:newUser.profilePic})
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                boyProfilePic: newUser.profilePic
+            })
         } else {
             return res.status(400).json({error: "Invalid user data"})
         }
 
     } catch (e) {
-        console.log("Error in sign up controller",e.message)
-       res.status(500).json({error: "Internal server Error"})
+        console.log("Error in sign up controller", e.message)
+        res.status(500).json({error: "Internal server Error"})
     }
 }
