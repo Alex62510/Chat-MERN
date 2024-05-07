e6a4259119e04e9946297e150787def548e54a39
Alex62510 <eagle625@tut.by>
2024-05-07 22:08:33 +0300

    add routes react-reouter-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,19 @@ import './App.css'
 import Login from "./components/login/Login.tsx";
 import Signup from "./components/signup/Signup.tsx";
 import Home from "./components/home/Home.tsx";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
 
 
     return (
         <div className={'p-4 h-screen flex items-center justify-center'}>
-            <Home/>
+            <Routes>
+                <Route path="/"  element={<Home/>}/>
+                <Route path="/login"  element={<Login/>}/>
+                <Route path="/signup"  element={<Signup/>}/>
+
+            </Routes>
         </div>
     )
 }
