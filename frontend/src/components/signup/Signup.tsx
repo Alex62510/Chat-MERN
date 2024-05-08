988368cfbea9504fb026c9a0af81ff2fbcceb093
Alex62510 <eagle625@tut.by>
2024-05-08 22:45:11 +0300

    add signup login routes

--- frontend/src/components/signup/Signup.tsx
+++ frontend/src/components/signup/Signup.tsx
@@ -1,6 +1,20 @@
 import GenderCheckBox from "./GenderCheckBox.tsx";
+import {Link} from "react-router-dom";
+import {FormEvent, useState} from "react";
 
 const Signup = () => {
+    const [inputs, setInputs] = useState({
+        fullname: '',
+        username: '',
+        password: '',
+        confirmpassword: '',
+        gender: ''
+    });
+
+    const handleSubmit=async (e: FormEvent<HTMLFormElement>)=>{
+        e.preventDefault()
+    }
+
     return (
         <div className={'flex flex-col items-center justify-center min-w-96 mx-auto'}>
             <div
@@ -8,7 +22,7 @@ const Signup = () => {
                 <h1 className={'text-3xl font-semibold text-center text-gray-100'}>Sign Up
                     <span className={'text-blue-500'}> ChatApp</span>
                 </h1>
-                <form className={''}>
+                <form className={''} onSubmit={handleSubmit}>
                     <div className={''}>
                         <label className={'label p-2 '}>
                             <span className={'text-base label-text text-gray-300 '}>
@@ -16,7 +30,10 @@ const Signup = () => {
                             </span>
                         </label>
                         <input type="text" placeholder={'Enter full name'}
-                               className={'w-full input input-bordered h-10'}/>
+                               className={'w-full input input-bordered h-10'}
+                               value={inputs.fullname} onChange={(e) => {
+                            setInputs({...inputs, fullname: e.target.value})
+                        }}/>
                     </div>
                     <div className={''}>
                         <label className={'label p-2 '}>
@@ -25,7 +42,10 @@ const Signup = () => {
                             </span>
                         </label>
                         <input type="text" placeholder={'Enter Username'}
-                               className={'w-full input input-bordered h-10'}/>
+                               className={'w-full input input-bordered h-10'}
+                               value={inputs.username} onChange={(e) => {
+                            setInputs({...inputs, username: e.target.value})
+                        }}/>
                     </div>
                     <div className={''}>
                         <label className={'label p-2 '}>
@@ -34,7 +54,10 @@ const Signup = () => {
                             </span>
                         </label>
                         <input type="password" placeholder={'Enter password'}
-                               className={'w-full input input-bordered h-10'}/>
+                               className={'w-full input input-bordered h-10'}
+                               value={inputs.password} onChange={(e) => {
+                            setInputs({...inputs, password: e.target.value})
+                        }}/>
                     </div>
                     <div className={'pb-2'}>
                         <label className={'label p-2 '}>
@@ -43,16 +66,20 @@ const Signup = () => {
                             </span>
                         </label>
                         <input type="password" placeholder={'Confirm password'}
-                               className={'w-full input input-bordered h-10'}/>
+                               className={'w-full input input-bordered h-10'}
+                               value={inputs.confirmpassword} onChange={(e) => {
+                            setInputs({...inputs, confirmpassword: e.target.value})
+                        }}/>
                     </div>
                     <GenderCheckBox/>
-                    <a href="#"
-                       className={'text-gray-300 text-sm hover:underline hover:text-blue-500 mt-2 inline-block'}>
-                       Already have an account?
-                    </a>
+                    <Link to="/login"
+                          className={'text-gray-300 text-sm hover:underline hover:text-blue-500 mt-2 inline-block'}>
+                        Already have an account?
+                    </Link>
                     <div className={''}>
                         <button
-                            className={'btn btn-block btn-sm mt-2 hover:opacity-70 '}>Sign up
+                            className={'btn btn-block btn-sm mt-2 hover:opacity-70 '}>Sign
+                            up
                         </button>
                     </div>
                 </form>
