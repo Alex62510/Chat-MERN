384ff6532effb829dee21b487b2c33c7632f85dc
Alex62510 <eagle625@tut.by>
2024-05-12 14:03:36 +0300

    create uselogout

--- frontend/src/components/sidebar/LogoutButton.tsx
+++ frontend/src/components/sidebar/LogoutButton.tsx
@@ -1,10 +1,13 @@
 import {BiLogOut} from "@react-icons/all-files/bi/BiLogOut";
+import useLogout from "../../hooks/useLogout.ts";
 
 
 const LogoutButton = () => {
+    const {logout, loading} = useLogout()
     return (
         <div className={'mt-auto'}>
-            <BiLogOut className={'w-6 h-6 text-white cursor-pointer hover:text-blue-500'} />
+            {!loading ? (<BiLogOut
+                className={'w-6 h-6 text-white cursor-pointer hover:text-blue-500'} onClick={logout}/>): (<span className={'loading loading-spinner'}></span>)}
         </div>
     );
 };
