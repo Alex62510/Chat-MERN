db28706f935484fe056090260a0f9739aa1220bf
Alex62510 <eagle625@tut.by>
2024-05-14 22:05:44 +0300

    add unmount messageContainer

--- frontend/src/components/messageContainer/MessageContainer.tsx
+++ frontend/src/components/messageContainer/MessageContainer.tsx
@@ -2,16 +2,26 @@ import Messages from "./Messages.tsx";
 import MessageInput from "./MessageInput.tsx";
 import NoChatSelected from "./NoChatSelected.tsx";
 import useConversation from "../../store/useConversation.ts";
+import {useEffect} from "react";
 
 
 const MessageContainer = () => {
     const {selectedConversation,setSelectedConversation}=useConversation()
+
+    useEffect(() => {
+        return ()=>{
+            //clean up
+            setSelectedConversation(null)
+        }
+    }, [setSelectedConversation]);
+
+
     return (
         <div className={'md:min-w-[450px] flex flex-col'}>
             {!selectedConversation ? <NoChatSelected/>:<>
                 <div className={'bg-slate-500 px-4 py-2 mb-2'}>
                     <span className={'label-text'}>To: </span>
-                    <span className={'text-gray-900 font-bold'}>User Name</span>
+                    <span className={'text-gray-900 font-bold'}>{selectedConversation.fullName}</span>
                 </div>
                 <Messages/>
                 <MessageInput/>
--- frontend/src/store/useConversation.ts
+++ frontend/src/store/useConversation.ts
@@ -3,14 +3,14 @@ import {ConversationType} from "../types/type.ts";
 
 type StateType = {
     selectedConversation: ConversationType | null
-    setSelectedConversation: (selectedConversation: ConversationType) => void,
+    setSelectedConversation: (selectedConversation: ConversationType|null) => void,
     messages: string[],
     setMessages: (messages: string[]) => void
 }
 
 const useConversation = create<StateType>((set) => ({
     selectedConversation: null,
-    setSelectedConversation: (selectedConversation: ConversationType) => set({selectedConversation}),
+    setSelectedConversation: (selectedConversation: ConversationType|null) => set({selectedConversation}),
     messages: [],
     setMessages: (messages: string[]) => set({messages}),
 }));
