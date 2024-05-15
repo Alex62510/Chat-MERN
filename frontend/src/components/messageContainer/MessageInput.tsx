01ea798c0df12bcba16ed8309d17b6f34c06ccc5
Alex62510 <eagle625@tut.by>
2024-05-15 21:44:07 +0300

    add useSendMessages

--- frontend/src/components/messageContainer/MessageInput.tsx
+++ frontend/src/components/messageContainer/MessageInput.tsx
@@ -1,14 +1,36 @@
 import {IoIosSend} from "@react-icons/all-files/io/IoIosSend";
+import {FormEvent, useState} from "react";
+import useSendMessage from "../../hooks/useSendMessage.ts";
 
 const MessageInput = () => {
+    const [message, setMessage] = useState<string>("");
+    const {sendMessage, loading} = useSendMessage()
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!message) {
+            return
+        }
+        await sendMessage(message);
+        setMessage('')
+    }
+
     return (
-        <form className={'px-4 my-3'}>
+        <form className={'px-4 my-3'} onSubmit={handleSubmit}>
             <div className={'w-full relative'}>
-                <input type="text" className={'border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white'}
-                placeholder={'Send a message'}
+                <input type="text"
+                       className={'border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white'}
+                       placeholder={'Send a message'}
+                       value={message}
+                       onChange={(e) => {
+                           setMessage(e.target.value)
+                       }}
                 />
-                <button type={'submit'} className={'absolute inset-y-0 end-0 flex items-center pe-3'}>
-                    <IoIosSend  className={'w-6 h-6 outline-none hover:text-blue-500'}/>
+                <button type={'submit'}
+                        className={'absolute inset-y-0 end-0 flex items-center pe-3'}>
+                    {loading ? <span className={'loading loading-spinner'}></span> :
+                        <IoIosSend
+                            className={'w-6 h-6 outline-none hover:text-blue-500'}/>}
                 </button>
             </div>
         </form>
