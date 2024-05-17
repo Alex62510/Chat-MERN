f44f6ab67f43b4c08e7b6cae07574c681dbe9cd4
Alex62510 <eagle625@tut.by>
2024-05-17 22:25:16 +0300

    add message ref

--- frontend/src/components/messageContainer/Messages.tsx
+++ frontend/src/components/messageContainer/Messages.tsx
@@ -1,14 +1,22 @@
 import useGetMessage from "../../hooks/useGetMessage.ts";
 import MessageSkeleton from "../skeletons/MessageSkeleton.tsx";
 import Message from "./Message.tsx";
+import {useEffect, useRef} from "react";
 
 const Messages = () => {
     const {messages, loading} = useGetMessage()
-    console.log(messages)
+    const lastMessageRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        setTimeout(()=>{
+            lastMessageRef.current?.scrollIntoView({})
+        },100)
+    }, [messages]);
     return (
         <div className={'px-4 flex-1 overflow-auto'}>
-            {!loading && messages.length > 0 &&messages.map((message) => (
-                <Message key={message._id} message={message}  />
+            {!loading && messages.length > 0 && messages.map((message) => (
+                <div key={message._id} ref={lastMessageRef}><Message message={message}/>
+                </div>
             ))}
             {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx}/>)}
             {!loading && messages.length === 0 &&
