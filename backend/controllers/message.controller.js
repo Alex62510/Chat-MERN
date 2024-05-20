329d4b74a20cefb34ff3d2207d966a1d73feca7a
Alex62510 <eagle625@tut.by>
2024-05-20 20:21:56 +0300

    add socketReceiver logic

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,7 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import {getReceiverSocketId} from "../socket/socket.js";
+import {io} from "socket.io-client";
 
 export const sendMessage = async (req, res) => {
     try {
@@ -24,9 +26,17 @@ export const sendMessage = async (req, res) => {
         if (newMessage) {
             conversation.messages.push(newMessage._id)
         }
+
+
         // await conversation.save()
         // await newMessage.save()
         await Promise.all([conversation.save(), newMessage.save()])
+
+        const receiverSocketId=getReceiverSocketId(receiverId)
+        if(receiverSocketId){
+            io.to(receiverSocketId).emit("newMessage",newMessage)
+        }
+
         res.status(200).json(newMessage)
     } catch (e) {
         console.log("Error in sendMessage controller", e.message)
