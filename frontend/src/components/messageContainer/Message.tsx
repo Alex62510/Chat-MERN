518726b2408df45303269c89b848fef24af53c64
Alex62510 <eagle625@tut.by>
2024-05-22 21:27:22 +0300

    fix app to deploy 5

--- frontend/src/components/messageContainer/Message.tsx
+++ frontend/src/components/messageContainer/Message.tsx
@@ -11,12 +11,12 @@ const Message = ({message}: Props) => {
     const {authUser} = useAuthContext()
     const {selectedConversation} = useConversation()
     const fromMe = message.senderId === authUser?._id
-    const formatedTime=extractTime(message.createdAt)
+    const formatedTime = extractTime(message.createdAt)
     const chatClassName = fromMe ? 'chat-end' : 'chat-start'
-    const profilePic=fromMe?authUser?.ProfilePic:selectedConversation?.profilePic
-    const bubbleBgColor=fromMe?"bg-blue-500":""
-    const shakeClass=message.shouldShake ?'shake':''
-
+    const profilePic = fromMe ? authUser?.profilePic : selectedConversation?.profilePic;
+    const bubbleBgColor = fromMe ? "bg-blue-500" : ""
+    const shakeClass = message.shouldShake ? 'shake' : ''
+console.log(profilePic)
     return (
         <div className={`chat ${chatClassName}`}>
             <div className={'chat-image avatar'}>
@@ -26,8 +26,10 @@ const Message = ({message}: Props) => {
                         alt="user avatar"/>
                 </div>
             </div>
-            <div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
-            <div className={'chat-footer text-xs opacity-50 flex gap-1 items-center'}>{formatedTime}
+            <div
+                className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
+            <div
+                className={'chat-footer text-xs opacity-50 flex gap-1 items-center'}>{formatedTime}
             </div>
 
         </div>
